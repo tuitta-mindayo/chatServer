52ad56c939d22ac6ec357263366b705482dd697e
びゅな <153439719+tuitta-mindayo@users.noreply.github.com>
2024-11-19 10:02:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let fs = require('fs');
 let http = require('http');
 let server = http.createServer((req, res) => {
-    fs.readFile('./index.html', 'UTF-8', (error, data) => {
+    fs.readFile('/index.html', 'UTF-8', (error, data) => {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         res.end();
