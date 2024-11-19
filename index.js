d1180a64a444ead4612892f8b4fa6268576a8afe
びゅな <153439719+tuitta-mindayo@users.noreply.github.com>
2024-11-19 10:28:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ let server = http.createServer((req, res) => {
 
 server.listen(3000);
 console.log('Start Server!');
+
+//getElements
+const name = document.getElementById('name');
+const description = document.getElementById('description');
+const addonType = document.getElementById('addonType');
+const minVersion = document.getElementById('minVersion');
+const make = document.getElementById('make');
+
+//EventListeners
+make.addEventListener('click', makeFile);
+
+function makeFile()
+{
+  alert("test");
+}
