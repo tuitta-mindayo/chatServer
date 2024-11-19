20994fbdb678b86c93ec1c02e676226b17c640b1
びゅな <153439719+tuitta-mindayo@users.noreply.github.com>
2024-11-19 10:09:10 +0900

    Create uievent.js

---
+++ uievent.js
@@ -0,0 +1,14 @@
+//getElements
+const name = document.getElementById('name');
+const description = document.getElementById('description');
+const addonType = document.getElementById('addonType');
+const minVersion = document.getElementById('minVersion');
+const make = document.getElementById('make');
+
+//EventListeners
+make.addEventListener('click', makeFile);
+
+function click()
+{
+  alert("test");
+}
